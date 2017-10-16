a2a4fe9fe7f80014eb23695aefeeeea87c3f1164
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2017-10-16 17:40:37 +0200

    Cleaned up the contents of modal.cmp, added a primary action btn

--- src/aura/modal/modalController.js
+++ src/aura/modal/modalController.js
@@ -1,19 +1,11 @@
 ({
-    openModel: function(component, event, helper) {
-        // for Display Model,set the "isOpen" attribute to "true"
-        component.set("v.isOpen", true);
-        document.body.style.overflow = "hidden";
+    openModal : function(component, event, helper) {
+        component.set('v.showModal', true);
+        document.body.style.overflow = 'hidden';
+        console.log('v.showModal=' + component.get('v.showModal'));
     },
-    closeModel: function(component, event, helper) {
-        // for Hide/Close Model,set the "isOpen" attribute to "Fasle"  
-        component.set("v.isOpen", false);
-        document.body.style.overflow = "auto";
-    },
-    likenClose: function(component, event, helper) {
-        // Display alert message on the click on the "Like and Close" button from Model Footer 
-        // and set set the "isOpen" attribute to "False for close the model Box.
-        alert('thanks for like Us :)');
-        component.set("v.isOpen", false);
-        document.body.style.overflow = "auto";
+    closeModal : function(component, event, helper) {
+        component.set('v.showModal', false);
+        document.body.style.overflow = 'auto';
     },
 })
