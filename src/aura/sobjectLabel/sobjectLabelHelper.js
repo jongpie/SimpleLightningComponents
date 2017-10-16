3e8b2b51d648be3f6382d5081df3418dc2c08c9b
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2017-10-16 17:40:47 +0200

    Code cleanup in sobjectLabelHelper.js

--- src/aura/sobjectLabel/sobjectLabelHelper.js
+++ src/aura/sobjectLabel/sobjectLabelHelper.js
@@ -1,12 +1,12 @@
 ({
     fetchSObjectMetadata : function(component, event) {
-        var action = component.get("c.getSObjectMetadata");
+        var action = component.get('c.getSObjectMetadata');
         action.setParams({
-            "sobjectName": component.get("v.sobjectName")
+            'sobjectName': component.get('v.sobjectName')
         });
         action.setStorable();
         action.setCallback(this, function(response) {
-            if (response.getState() == "SUCCESS") {
+            if(response.getState() == 'SUCCESS') {
                 var sobjectMetadata = response.getReturnValue();
                 component.set('v.sobjectMetadata', sobjectMetadata);
                 component.set('v.label', sobjectMetadata.sobjectLabel);
