b2d14b718a984cb86fcae4fd47acb74425f9a636
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2017-10-17 18:14:31 +0200

    Added doneRendering function in dataTable's controller

--- src/aura/dataTable/dataTableController.js
+++ src/aura/dataTable/dataTableController.js
@@ -1,5 +1,8 @@
 ({
     doInit : function(component, event, helper) {
         helper.fetchFieldSetMetadata(component, event);
+    },
+    doneRendering : function(component, event, helper) {
+        component.set('v.isDoneRendering', true);
     }
 })
