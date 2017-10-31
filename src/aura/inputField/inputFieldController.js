0bcacf9542250c8077ca747c634552e7bfd966c3
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2017-10-31 02:02:34 +0100

    Add more functionality to c:inputField, cleaned up code

--- src/aura/inputField/inputFieldController.js
+++ src/aura/inputField/inputFieldController.js
@@ -1,5 +1,6 @@
 ({
     doInit : function(component, event, helper) {
+        helper.fetchSObjectMetadata(component, event);
         helper.fetchFieldMetadata(component, event);
         helper.parseFieldValue(component, event);
         helper.parsePicklistOptions(component, event);
@@ -11,26 +12,9 @@
         component.set('v.fieldValue', record[fieldName]);
     },
     handleFieldValueChanged : function(component, event, helper) {
-        var changedField  = component.get('v.fieldName');
-        var record        = component.get('v.record');
-        var fieldValue    = component.get('v.fieldValue');
-        var fieldMetadata = component.get('v.fieldMetadata');
-        var fieldType     = component.get('v.fieldType');
-
-        var newFieldValue = event.getParam('value') != undefined ? event.getParam('value') : event.getSource().get('v.value');
-        if(typeof newFieldValue == 'undefined') newFieldValue = '';
-        var oldFieldValue = event.getParam('oldValue') != undefined ? event.getParam('oldValue') : event.getSource().get('v.oldValue');
-
-        if(newFieldValue != oldFieldValue) {
-            if(fieldMetadata != null && fieldType != fieldMetadata.fieldType && typeof fieldType == 'string') {
-                newFieldValue = newFieldValue.toString();
-            }
-            record[changedField] = newFieldValue;
-            component.set('v.record', record);
-            helper.parsePicklistOptions(component, event);
-        }
+        helper.handleFieldValueChanged(component, event);
     },
     handleBlur : function(component, event, helper) {
-        helper.validateFieldValue(component, event, helper)
+        helper.validateFieldValue(component, event)
     }
 })
--- src/aura/inputField/inputFieldHelper.js
+++ src/aura/inputField/inputFieldHelper.js
@@ -1,40 +1,47 @@
 ({
-    fetchFieldMetadata : function(component, event) {
-        var fieldName = component.get('v.fieldName');
-        var action = component.get('c.getFieldMetadata');
+    fetchSObjectMetadata : function(component, event) {
+        var action = component.get('c.getSObjectMetadata');
         action.setParams({
-            'sobjectName': component.get('v.sobjectName'),
-            'fieldName': fieldName
+            'sobjectName': component.get('v.sobjectName')
         });
         action.setStorable();
         action.setCallback(this, function(response) {
-            if(response.getState() == 'SUCCESS') {
-                var fieldMetadata = response.getReturnValue();
-                component.set('v.fieldMetadata', fieldMetadata);
-
-                if(component.get('v.displayType') == null) {
-                    component.set('v.displayType', fieldMetadata.displayType);
-                }
-                if(component.get('v.disabled') == null) {
-                    component.set('v.disabled', fieldMetadata.fieldLevelSecurity.isUpdateable == false);
-                }
-                if(component.get('v.required') == null) {
-                    component.set('v.required', fieldMetadata.required);
-                }
-                this.parsePicklistOptions(component, event);
+            if(response.getState() === 'SUCCESS') {
+                var sobjectMetadata = response.getReturnValue();
+                component.set('v.sobjectMetadata', sobjectMetadata);
             } else {
-                console.log('ERROR');
-                for(var i=0; i < response.getError().length; i++) {
+                console.log(response.getError().length + ' ERRORS');
+                for(var i = 0; i < response.getError().length; i++) {
                    console.log(response.getError()[i]);
                 }
             }
         });
         $A.enqueueAction(action);
     },
-    applyInputOptionOverrides : function(component, event) {
-        var inputOptions = component.get('v.inputOptions');
-        component.find('uiInputField');
-        //Object.assign(obj1, obj2);
+    fetchFieldMetadata : function(component, event) {
+        var action = component.get('c.getFieldMetadata');
+        action.setParams({
+            'sobjectName': component.get('v.sobjectName'),
+            'fieldName': component.get('v.fieldName')
+        });
+        action.setStorable();
+        action.setCallback(this, function(response) {
+            if(response.getState() === 'SUCCESS') {
+                var fieldMetadata = response.getReturnValue();
+                component.set('v.fieldMetadata', fieldMetadata);
+
+                if(component.get('v.displayType') === undefined) {
+                    component.set('v.displayType', fieldMetadata.displayType);
+                }
+                this.parsePicklistOptions(component, event);
+            } else {
+                console.log(response.getError().length + ' ERRORS');
+                for(var i = 0; i < response.getError().length; i++) {
+                   console.log(response.getError()[i]);
+                }
+            }
+        });
+        $A.enqueueAction(action);
     },
     parseFieldValue : function(component, event) {
         var record = component.get('v.record');
@@ -58,21 +65,35 @@
         }
         component.set('v.picklistOptions', picklistOptions);
     },
-    validateFieldValue : function(component, event) {
-        var fieldRequired = component.get('v.required');
-
-        if(fieldRequired == false) return;
-
+    handleFieldValueChanged : function(component, event) {
         var changedField  = component.get('v.fieldName');
         var record        = component.get('v.record');
         var fieldValue    = component.get('v.fieldValue');
         var fieldMetadata = component.get('v.fieldMetadata');
         var fieldType     = component.get('v.fieldType');
 
-        var fieldValueMissing = fieldRequired && (fieldValue == null || fieldValue == '');
+        var newFieldValue = event.getParam('value') != undefined ? event.getParam('value') : event.getSource().get('v.value');
+        if(typeof newFieldValue == 'undefined') newFieldValue = '';
+        var oldFieldValue = event.getParam('oldValue') != undefined ? event.getParam('oldValue') : event.getSource().get('v.oldValue');
+
+        if(newFieldValue != oldFieldValue) {
+            if(fieldMetadata != null && fieldType != fieldMetadata.fieldType && typeof fieldType == 'string') {
+                newFieldValue = newFieldValue.toString();
+            }
+            record[changedField] = newFieldValue;
+            component.set('v.record', record);
+            this.parsePicklistOptions(component, event);
+        }
+    },
+    validateFieldValue : function(component, event) {
+        if(component.get('v.required') == false) return;
+
+        var fieldValue    = component.get('v.fieldValue');
+
+        var fieldValueMissing = fieldValue == null || fieldValue == '' || fieldValue == undefined;
         if(fieldValueMissing) {
             var inputField = component.find('inputField');
-            if(inputField) inputField.set('v.errors', [{message:'The field is required'}]);
+            if(inputField) inputField.set('v.errors', [{message:'This field is required'}]);
         }
     }
 })
