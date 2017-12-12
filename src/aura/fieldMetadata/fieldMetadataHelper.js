aa17b1c03b224da840ac72b58bb6b60438070826
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2017-12-12 10:48:45 +0100

    inputField now extends the new abstract component, fieldMetadata.cmp

---
+++ src/aura/fieldMetadata/fieldMetadataHelper.js
@@ -0,0 +1,25 @@
+({
+    fetchFieldMetadata : function(component, event) {
+        var action = component.get('c.getFieldMetadata');
+        action.setParams({
+            'sobjectName': component.get('v.sobjectName'),
+            'fieldName': component.get('v.fieldName')
+        });
+        action.setStorable();
+        action.setCallback(this, function(response) {
+            if(response.getState() === 'SUCCESS') {
+                component.set('v.fieldMetadata', response.getReturnValue());
+                component.set('v.label', response.getReturnValue().label);
+            } else if(response.getState() === 'ERROR') {
+                this.processCallbackErrors(response);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    processCallbackErrors : function(response) {
+        console.log('ERROR');
+        for(var i=0; i < response.getError().length; i++) {
+           console.log(response.getError()[i]);
+        }
+    }
+})
