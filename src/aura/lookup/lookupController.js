baac134bfbb7ab8a760ac7a5479480964113852a
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2018-01-08 17:47:10 +0100

    Cut out server calls by using the SObject Metadata returned in fieldMetadata.relationshipReferences

--- src/aura/lookup/lookupController.js
+++ src/aura/lookup/lookupController.js
@@ -1,49 +1,56 @@
 ({
     doInit :  function(component, event, helper) {
-        console.log('lookup doInit');
         var fieldMetadata = component.get('v.fieldMetadata');
 
         if(!fieldMetadata) return;
 
-        component.set('v.parentSObjectName', fieldMetadata.relationshipReferences[0].name);
+        var defaultRelationshipReference;
+        for(var i = 0; i < fieldMetadata.relationshipReferences.length; i++) {
+            var relationshipReference = fieldMetadata.relationshipReferences[i];
+            if(relationshipReference.isAccessible === true) {
+                defaultRelationshipReference = relationshipReference;
+                break;
+            }
+        }
+        component.set('v.parentSObjectName', defaultRelationshipReference.name);
     },
     toggleParentSObjectSelector : function(component, event, helper) {
-        console.log('lookup toggleSObjectSelector');
         component.set('v.showSObjectSelector', !component.get('v.showSObjectSelector'));
         component.set('v.showSearchResults', !component.get('v.showSearchResults'));
     },
     selectParentSObject : function(component, event, helper) {
-        console.log('lookup selectParentSObject');
         var parentSObjectName = event.currentTarget.dataset.sobjectname;
-        console.log('parentSObjectName=' + parentSObjectName);
-        component.set('v.parentSObjectMetadata', null);
         component.set('v.parentSObjectName', parentSObjectName);
-        component.set('v.showSObjectSelector', false);
     },
-    fetchParentSObjectMetadata :  function(component, event, helper) {
+    loadParentSObjectMetadata : function(component, event, helper) {
+        component.set('v.showSearchResults', null);
+        component.set('v.showSObjectSelector', false);
+
+        var fieldMetadata = component.get('v.fieldMetadata');
         var parentSObjectName = component.get('v.parentSObjectName');
 
-        var sobjectMetadataService = component.find('sobjectMetadataService');
-        sobjectMetadataService.set('v.sobjectName', parentSObjectName);
-        sobjectMetadataService.fetch(function(error, data) {
-            component.set('v.parentSObjectMetadata', data);
-        });
+        var selectedSObjectMetadata;
+        for(var i = 0; i < fieldMetadata.relationshipReferences.length; i++) {
+            var relationshipReference = fieldMetadata.relationshipReferences[i];
+
+            if(relationshipReference.name !== parentSObjectName) continue;
+
+            selectedSObjectMetadata = fieldMetadata.relationshipReferences[i];
+            break;
+        }
+        component.set('v.parentSObjectMetadata', selectedSObjectMetadata);
     },
     fetchSearchResults :  function(component, event, helper) {
-        console.log('fetchSearchResults');
         helper.fetchSearchResults(component, event, helper);
         component.set('v.showSearchResults', true);
     },
     hideSearchResults : function(component, event, helper) {
-        console.log('hideSearchResults');
         component.set('v.showSearchResults', false);
     },
     itemSelected : function(component, event, helper) {
-        console.log('itemSelected');
         helper.itemSelected(component, event, helper);
     },
     clearSelection : function(component, event, helper) {
-        console.log('clearSelection');
         helper.clearSelection(component, event, helper);
     }
 })
