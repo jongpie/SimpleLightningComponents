8607eb904a73b71a450c0f69dccb72b3bbc1521b
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2018-03-19 12:42:56 +0100

    outputField.cmp now shows picklist translations instead of the raw value
    This works for picklists & multi-select picklists

--- src/aura/outputField/outputFieldController.js
+++ src/aura/outputField/outputFieldController.js
@@ -1,17 +1,21 @@
 ({
     doInit : function(component, event, helper) {
         helper.handleFieldValueChanged(component, event);
+        helper.getPicklistLabels(component, event);
     },
     handleFieldMetadataChanged : function(component, event, helper) {
         helper.setFieldMetadataAttributes(component, event);
+        helper.getPicklistLabels(component, event);
     },
     handleRecordChanged : function(component, event, helper) {
         var record       = component.get('v.record');
         var fieldApiName = component.get('v.fieldApiName');
 
         component.set('v.fieldValue', record[fieldApiName]);
+        helper.getPicklistLabels(component, event);
     },
     handleFieldValueChanged : function(component, event, helper) {
         helper.handleFieldValueChanged(component, event);
+        helper.getPicklistLabels(component, event);
     }
 })
