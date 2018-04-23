d7a4d2daee172f6f7b0ce1b1aacba8d878adbf78
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2018-04-23 10:34:42 +0100

    Made attributes optional so empty cmp can be used, added render logic & new hide logic

--- src/aura/toastNotification/toastNotificationController.js
+++ src/aura/toastNotification/toastNotificationController.js
@@ -1,10 +1,15 @@
 ({
     closeNotification : function(component, event, helper) {
-        component.destroy();
+        component.set('v.render', false);
+        component.set('v.type', null);
+        component.set('v.title', null);
+        component.set('v.details', null);
     },
     handleNotificationEvent : function(component, event, helper) {
+        var hasType = event.getParam('type') !== null;
+        component.set('v.render', hasType);
         component.set('v.type', event.getParam('type'));
-        component.set('v.message', event.getParam('message'));
+        component.set('v.title', event.getParam('title'));
         component.set('v.details', event.getParam('details'));
     }
 })
