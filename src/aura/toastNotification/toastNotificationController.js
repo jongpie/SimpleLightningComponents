fb1e042b6c21e10f1e052a760f206cbd4352ed23
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2018-04-23 09:57:54 +0100

    Renamed a function

--- src/aura/toastNotification/toastNotificationController.js
+++ src/aura/toastNotification/toastNotificationController.js
@@ -2,7 +2,7 @@
     closeNotification : function(component, event, helper) {
         component.destroy();
     },
-    handleNotification : function(component, event, helper) {
+    handleNotificationEvent : function(component, event, helper) {
         component.set('v.type', event.getParam('type'));
         component.set('v.message', event.getParam('message'));
         component.set('v.details', event.getParam('details'));
